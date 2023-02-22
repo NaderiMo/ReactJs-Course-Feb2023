1121b2d95c46a8a06107cc75c6613d13c70a047c
nargeshadi <narges_hadi666@yahoo.com>
2023-02-22 15:12:18 +0330

    hello

--- src/Course.js
+++ src/Course.js
@@ -5,6 +5,7 @@ function Course(props){
             <h1>{props.title}</h1>
             <h2>{props.price}</h2>
             <p>{props.description}</p>
+            <h1></h1>
         </div>
     )
 }
