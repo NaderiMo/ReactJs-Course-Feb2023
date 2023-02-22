719b02a129f0098e5b66c3f99471e592e3bfece4
NahidNasirii <Academy@Behsazan.net>
2023-02-22 15:07:12 +0330

    course3-2

---
+++ src/Course.js
@@ -0,0 +1,10 @@
+function Course({ tittle, price, description }) {
+  return (
+    <div>
+      <h1>{tittle}</h1>
+      <h2>{price}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+export default Course;
