e7f953fe0ae7dfa4d6001bd8811baab0a1925ff1
fatima majidi <f.majidi2010@gmail.com>
2023-02-26 15:03:22 +0330

    session4-listOfCards-stats

---
+++ src/component/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card({cardNamber,type}){
+    return (<div>
+        <h1>{cardNamber}</h1>
+<p>{type}</p>
+<br/>
+       </div>
+        )
+}
+
+export default Card;
---
+++ src/component/CardList.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Card from "./Card";
+
+
+
+
+function CardList({}){
+
+    const firstListofCards = [
+        {cardNumber:"5022", type:"1"},
+        {cardNumber:"5023", type:"1"},
+        {cardNumber:"5024", type:"1"}
+        ]
+
+const [cardsState,setCards] = useState(firstListofCards)
+
+const newCard =  [
+    {cardNumber:"1212", type:"1"},
+    {cardNumber:"2222", type:"1"},
+    {cardNumber:"23434", type:"1"}
+    ]
+        const loadMore = ()=>{
+            setCards([
+                ...firstListofCards,...newCard
+            ])
+        }
+
+
+    // console.log(cards.length);
+    return (
+    <div>
+        {
+        cardsState.map(
+            (item)=>
+            (
+            <Card key={item.cardNamber}
+        cardNamber={item.cardNumber}
+        type={item.type} 
+        ></Card>))}
+        <button onClick={()=>loadMore()} >click for more</button>
+    </div>)
+}
+export default CardList;
