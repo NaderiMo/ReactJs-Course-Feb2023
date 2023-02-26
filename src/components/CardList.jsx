a0587c17bb8ac5f1463d664cd00af33386afb2b1
Mohsen <test@test.test>
2023-02-26 15:16:39 +0330

    Session4

---
+++ src/components/CardList.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Card from "./Card";
+
+
+function CardList({ }) {
+
+    const firstListOfCards = [
+        {
+            cardNumber: "00000000000", type: "بن کارت"
+        }, {
+            cardNumber: "11111111111", type: "کارت معمولی"
+        }, {
+            cardNumber: "22222222222", type: "بن کارت"
+        }]
+    const newCards = [
+        {
+            cardNumber: "3333", type: "بن کارت"
+        }, {
+            cardNumber: "44444", type: "کارت معمولی"
+        }, {
+            cardNumber: "55555", type: "بن کارت"
+        }]
+
+    const [cardsState, setCards] = useState(firstListOfCards)
+
+    const loadMore = () => {
+        setCards([...firstListOfCards, ...newCards])
+    }
+
+    return (
+        <div>
+            {
+                cardsState.map(
+                    (item) => (
+                        <Card
+                            cardNumber={item.cardNumber}
+                            type={item.type}
+                        />)
+                )
+            }
+            <button onClick={() => loadMore()}>Load More</button>
+        </div>)
+}
+export default CardList;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CardList from './components/CardList';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CardList />
   </React.StrictMode>
 );
 
