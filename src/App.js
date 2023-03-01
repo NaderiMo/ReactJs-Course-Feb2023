d86cf9a812dd3b1cc2d947315cf1877ca97c3a11
fatimamajidi <f.majidi2010@gmail.com>
2023-03-01 13:42:21 -0800

    clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import CardList from './components/CardList';
+import "./App.css";
+import CardList from "./components/CardList";
+import Clock from "./components/Clock";
 
 function App() {
   return (
     <div className="App">
       <CardList></CardList>
+      <Clock />
     </div>
   );
 }
