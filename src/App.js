5cc337c876b61af9422aef0f1e42769a6e2a81c5
abedinejad <aliyehabedi@gmail.com>
2023-03-01 14:36:52 +0330

    counter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
 import CardList from './components/CardList';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
         <Clock/>
         <CardList />
+        <Counter/>
     </div>
   );
 }
