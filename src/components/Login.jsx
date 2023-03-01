a2a92a8e16a3538ceaf4b2a3049bd9be27dd0ac4
pghasemian <ghasemianparisa1995@gmail.com>
2023-03-01 15:20:09 -0800

    Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+function Login({ }) {
+
+    return (<>
+        <Form.Label htmlFor="inputpasswords">Password</Form.Label>
+        <Form.Control
+            type="text"
+            id="userName"
+        />
+        <Form.Control
+            type="password"
+            id="password"
+        />
+        <Button>Login</Button>
+    </>)
+
+}
+export default Login;
