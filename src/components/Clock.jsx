c2d48ff32a4b476581bae3adb6806403da903cea
Mohsen <test@test.test>
2023-03-01 14:19:25 +0330

    clock style

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-
+import "./Clock.css"
 function Clock() {
     const [date, setDate] = useState(new Date())
-    
+
     setInterval(() => {
         setDate(new Date())
     }, 1000);
@@ -11,9 +11,11 @@ function Clock() {
     // setInterval(() => {
     //     date = new Date()
     // }, 1000);
+    
+    const h1Style = { backgroundColor: '#ffcc00' }
 
     return (<div>
-        <h1>{date.toLocaleTimeString()}</h1>
+        <h1 style={h1Style} className="clock">{date.toLocaleTimeString()}</h1>
     </div>)
 }
 export default Clock;
