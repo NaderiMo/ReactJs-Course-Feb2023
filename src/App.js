9dca1b78f426a9cee8a88010d519565b69e031a1
fatimamajidi <f.majidi2010@gmail.com>
2023-03-01 14:21:11 -0800

    Counter  Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CardList from "./components/CardList";
 import Clock from "./components/Clock";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <CardList></CardList>
       <Clock />
+      <Counter />
     </div>
   );
 }
