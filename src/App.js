389c3dd6cf9c304039925cab9955605603c910b8
abedinejad <aliyehabedi@gmail.com>
2023-03-01 14:25:05 +0330

    cardlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
+import CardList from './components/CardList';
 
 function App() {
   return (
     <div className="App">
         <Clock/>
+        <CardList />
     </div>
   );
 }
