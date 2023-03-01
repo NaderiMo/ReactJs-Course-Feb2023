6bb8de728b509399e9a2ca24c7f4fa551d542b5e
razimnejad <azimnejad.r@gmail.com>
2023-03-01 13:37:21 +0330

    add-clock-component

---
+++ src/components/Clock.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Clock() {
+    const [date, setDate] = useState(new Date());
+    setInterval(() => {
+        setDate(new Date())
+    }, 1000);
+    return(
+        <div>
+            <h1>{date.toLocaleTimeString()}</h1>
+        </div>
+    )
+}
+
+export default Clock;
