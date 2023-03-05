35b09c212c90b2f701b0bcc84b84412973413e8d
nargeshadi <Academy@Behsazan.net>
2023-03-05 14:43:26 +0330

    7aession

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,18 +1,53 @@
-import {useState} from "react";
-import {Button, Form, FormLabel } from 'react-bootstrap';
+import { useState } from "react";
+import { Button, Form, FormLabel } from "react-bootstrap";
 
 function Login() {
-    return (<>
-    <FormLabel htmlFor="inputPassword5"></FormLabel>
-    <Form.Control
-    type="text"
-    id="userName"
-    />
-    <Form.Control
-    type="password"
-    id="password"
-    />
-    <Button>Login</Button>
-    </>)
+  const [loginState, setLoginState] = useState({
+    username: null,
+    password: null
+  });
+  const [errorState, setErrorState] = useState({
+    username: "",
+    password: ""
+  });
+  const onChangeUsername = event => {
+    setLoginState({ ...loginState, password: event.target.value });
+  };
+  const submit = () => {
+    if (!loginState.username) {
+      setErrorState({ username: "Invalid username!", password: "" });
+      return;
     }
-    export default Login;
+    if (!loginState.password) {
+      setErrorState({ username: "", password: "Invalid username!" });
+      return;
+    }
+    setErrorState({ username: "", password: "" });
+    console.log("loginState", loginState);
+  };
+
+  return (
+    <>
+      <FormLabel htmlFor="inputPassword5"></FormLabel>
+      <Form.Control
+        type="text"
+        id="userName"
+        onChange={event => onChangeUsername(event)}
+      />
+      <Form.Control
+        type="password"
+        id="password"
+        onChange={event => onChangePassword(event)}
+      />
+      {errorState.password && (
+        <p className="text-danger">{errorState.password}</p>
+      )}
+      <Button onClick={() => submit()}>Login</Button>
+      <hr />
+      {loginState.username}
+      <hr />
+      {loginState.password}
+    </>
+  );
+}
+export default Login;
