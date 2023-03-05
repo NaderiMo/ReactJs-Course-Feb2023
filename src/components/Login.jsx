f7561f9f116dc971a988ad48e528e35455f3400b
mona <mona.shahi@me.com>
2023-03-05 14:58:46 +0330

    6thcommite

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,57 @@
+import axios from "axios";
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
 
 function Login()
 {
+    const[loginState,setLoginState] = useState({
+        username :"" ,
+        password : ""
+    })
+
+    const[errorState,setErrorState] = useState({
+        username :"" ,
+        password : ""
+    })
+
+    const defaultErrorState = {
+        username :null ,
+        password : null
+    }
+        
+    const onChangeUsername = (event) =>{
+        setLoginState ({...loginState,username: event.target.value})
+    }
+
+    const onChangePassword = (event) =>{
+        setLoginState ({...loginState,password: event.target.value})
+    }
+
+    const submit = () => {
+        if (!loginState.username) {   
+            setErrorState({ username :"invalid username" ,password :"" })
+            return;
+        }
+        if (!loginState.password) {
+        setErrorState({ username :"" , password :"invalid password"})
+            return;
+        }
+        //  setErrorState(defaultErrorState)
+
+        setErrorState({
+            username : "" ,
+            password : ""
+
+        })
+
+        axios.post("https://6404753c3bdc59fa8f39554b.mockapi.io/users",loginState)
+        .then((res)=> {debugger;})
+        .catch((error) => {debugger;})
+
+    }
+
+
 
 
     return(<>
@@ -10,14 +59,21 @@ function Login()
         <Form.Control
         type="text"
         id="userName"
+        onChange={(event) => onChangeUsername(event)}
            />
 
        <Form.Control
         type="password"
         id="password"
+        onChange={(event) => onChangePassword(event)}
            />
 
-       <Button>Login</Button>
+         {errorState.username  && <p className = "text-danger"> {errorState.username} </p>} 
+         {errorState.password  && <p className = "text-danger"> {errorState.password} </p>}   
+
+       <Button onClick={()=> submit()}>Login</Button><hr />
+       {loginState.username}<hr />
+        {loginState.password}
     </>)
 }
 export default Login;
