d173325ab3d444ac6e5048d8a59efd227d436b65
Nahid.Nasirii <Nahid.Nasiri0007@gmail.com>
2023-03-05 14:57:02 +0330

    Session6_Nasiri

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,52 @@
 
+import axios from 'axios';
+import { useState } from 'react';
 import { Button ,Form} from 'react-bootstrap';
 function Login(){
+    const [Loginstate,setLoginstate]=useState({
+        userName: "",
+        passWord: ""
+    })
+
+    const [errorstate,setErrorstate]=useState({
+        userName: "",
+        passWord: ""
+    })
+
+    const onchangeUserName=(event) => {
+        setLoginstate({...Loginstate,userName:event.target.value})
+
+    }
+    const onchangePassWord=(event) => {
+        setLoginstate({...Loginstate,passWord:event.target.value})
+    }
+
+    const submit=() => {
+      
+        if(!Loginstate.userName){
+            setErrorstate({userName:"InvalidUserName" , passWord:""});
+            return;
+        }
+        if(!Loginstate.passWord){
+            setErrorstate({userName:"" , passWord:"InvalidPassWord"});
+            return;
+        }
+        setErrorstate({userName:"",passWord:""})
+
+        axios.post("https://6404753c3bdc59fa8f39554b.mockapi.io/users",Loginstate).
+        then((res) => {debugger;}).catch((error)=>{debugger;})
+    }
 
     return(
         <>
-        
+       
         <Form.Label>UserName</Form.Label>
-        <Form.Control type="text" placeholder="UserName" />
+        <Form.Control type="text" placeholder="UserName"  onChange={(event)=> onchangeUserName(event)}/>
+        {errorstate.userName && <p className='text-danger'>{errorstate.userName}</p>}
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-
-        <Button>Login</Button>
+        <Form.Control type="password" placeholder="Password" onChange={(event)=> onchangePassWord(event)}/>
+        {errorstate.passWord && <p className='text-warningn'>{errorstate.passWord}</p>}
+        <Button onClick={()=> submit()}>Login</Button>
         </>
     )
 }
