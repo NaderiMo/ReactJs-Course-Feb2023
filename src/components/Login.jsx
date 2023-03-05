caef36892b8cb238646d4ed709573de11c57e13f
fatimamajidi <f.majidi2010@gmail.com>
2023-03-05 15:05:08 -0800

    axios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,89 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { click } from "@testing-library/user-event/dist/click";
+import axios from "axios";
 
 export default function Login() {
-  const [name, setUserName] = useState();
+  const [loginState, setLoginSate] = useState({
+    userName: null,
+    passWord: null,
+  });
 
-  const clickHandler = (userName) => {
-    console.log(userName);
-    setUserName(userName);
+  const defaultSate = { userName: "", passWord: "" };
+
+  const [errorState, setErrorState] = useState({ defaultSate });
+
+  const [responseState, setResponseSate] = useState({
+    responseCode: "",
+    responseDesc: "",
+  });
+
+  const validateLogin = () => {
+    if (!loginState.userName) {
+      setErrorState({ userName: "userName is invalid" });
+      return false;
+    }
+    // setErrorState(defaultSate);
+
+    if (!loginState.passWord) {
+      setErrorState({ passWord: "passWord is invalid" });
+      return false;
+    }
+    setErrorState(defaultSate);
+    return true;
+  };
+
+  const submit = () => {
+    if (!validateLogin()) return;
+
+    axios
+      .post("https://6404753c3bdc59fa8f39554b.mockapi.io/users", loginState)
+      .then((res) => {
+        console.log(res);
+        // setResponseSate(res);
+      })
+      .catch((res) => {
+        console.log(res);
+        // setResponseSate(res);
+      });
+  };
+
+  const onUserNameChange = (event) => {
+    setLoginSate({ ...loginState, userName: event.target.value });
+  };
+
+  const onPassWordChange = (event) => {
+    setLoginSate({ ...loginState, passWord: event.target.value });
   };
   return (
     <>
       <Form.Label htmlFor="inputPassword5">userName</Form.Label>
-      <Form.Control type="text" id="userName" />
+      <Form.Control
+        type="text"
+        id="userName"
+        onChange={(event) => {
+          onUserNameChange(event);
+        }}
+      />
+      {errorState.userName && (
+        <p className="text-danger">{errorState.userName}</p>
+      )}
 
       <Form.Label>passWord</Form.Label>
-      <Form.Control type="passWord" id="passWord" />
+      <Form.Control
+        type="passWord"
+        id="passWord"
+        onChange={(event) => {
+          onPassWordChange(event);
+        }}
+      />
+      {errorState.passWord && (
+        <p className="text-danger">{errorState.passWord}</p>
+      )}
       <Button
-      // onClick={() => {
-      //   clickHandler(userName);
-      // }}
+        onClick={() => {
+          submit();
+        }}
       >
         Login
       </Button>
