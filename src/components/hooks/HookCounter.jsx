42c5ab0f8274daf7ba85d9de37a9465ca3726b8f
fatimamajidi <f.majidi2010@gmail.com>
2023-04-10 15:57:11 -0700

    402

---
+++ src/components/hooks/HookCounter.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+export default function HookCounter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {});
+
+  return <div>Counter</div>;
+}
--- src/configs/router.js
+++ src/configs/router.js
@@ -1,47 +1,56 @@
-
-import { createBrowserRouter, Outlet } from "react-router-dom"
-import Login from "../components/Login"
-import CardList from '../components/CardList';
-import Clock from '../components/Clock';
-import Counter from '../components/Counter';
-import { Container, Navbar, NavDropdown, Nav, Col ,Row} from "react-bootstrap";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import CardList from "../components/CardList";
+import Clock from "../components/Clock";
+import Login1 from "../components/Login1";
+import Counter from "../components/Counter";
+import { Container, Navbar, NavDropdown, Nav, Col, Row } from "react-bootstrap";
+import HookCounter from "../components/hooks/HookCounter";
 const router = createBrowserRouter([
-    {
-        path: "/", element: (
-            <>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/login">Login</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                <Container >
-                    <Outlet />
-                </Container>
-                <Container >
-                    <Row>
-                        <Col>Col 1</Col>
-                        <Col>Col 2</Col>
-                        <Col>Col 3</Col>
-                        <Col>Col 4</Col>
-                    </Row>
-                </Container>
-            </>
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container>
+          <Outlet />
+        </Container>
+        <Container>
+          <Row>
+            <Col>Col 1</Col>
+            <Col>Col 2</Col>
+            <Col>Col 3</Col>
+            <Col>Col 4</Col>
+          </Row>
+        </Container>
+      </>
+    ),
+    children: [
+      { path: "/login", element: <Login1 /> },
+      {
+        path: "/signup",
+        element: (
+          <div>
+            <h1>SignUp</h1>
+          </div>
         ),
-        children: [
-            { path: "/login", element: (<Login />) },
-            { path: "/signup", element: (<div><h1>SignUp</h1></div>) }
-        ]
-    },
-    { path: "/CardList", element: (<CardList />) },
-    { path: "/Clock", element: (<Clock />) },
-    { path: "/Counter", element: (<Counter />) },
-])
+      },
+    ],
+  },
+  { path: "/CardList", element: <CardList /> },
+  { path: "/Clock", element: <Clock /> },
+  { path: "/Counter", element: <Counter /> },
+  { path: "/HookCounter", element: <HookCounter /> },
+]);
 
-export default router
+export default router;
