18510d7cc2f91822f7e80947799990d446d6a717
Mohsen <test@test.test>
2023-04-30 11:49:56 +0430

    install nextjs

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card({cardNumber,type}) {
-    return (<div>
-        <h1>{cardNumber}</h1>
-        <p>{type}</p>
-        <hr /> 
-    </div>)
-}
-export default Card;
- 
--- src/components/hooks/useContext/ComponentZ.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import DataContext from "../../../context/DataContext";
-
-export default function ComponentZ() {
-    const [user, setUser] = useContext(DataContext)
-    return (<>Z {user}
-        <button onClick={() => setUser("Mohsen Naderi")}>Change Context data</button>
-    </>)
-}
